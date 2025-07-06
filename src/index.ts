b01b81ccf4d211440d5ca28c75f540e69b5b7542
KirillYusikov <109421695+Kirill20poshliy@users.noreply.github.com>
2025-07-07 01:08:37 +0300

    feat: task 3 has been completed

--- src/index.ts
+++ src/index.ts
@@ -79,3 +79,33 @@ const normalizeData = (unnormalizedData: IPost[]): INormalizedData => {
 
 const normalizedPosts = normalizeData(posts);
 console.log(normalizedPosts);
+
+// Task #3
+
+interface IComment {
+  id: number;
+  postId: number;
+  email: string;
+  name: string;
+  body: string;
+}
+
+const COMMENTS_URL = 'https://jsonplaceholder.typicode.com/comments';
+
+const getData = async (url: string): Promise<IComment[]> => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return await response.json() as IComment[];
+};
+
+getData(COMMENTS_URL)
+  .then(data => {
+    data.forEach(comment => {
+      console.log(`ID: ${comment.id}, Email: ${comment.email}`);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
